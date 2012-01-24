9c0e1b6b2f1f24d76fe068788289b728181dea7c
David Antosh <jivebird@gmail.com>
2012-01-24 13:30:18 -0500

    Added argument detection to mocks.

--- lust.lua
+++ lust.lua
@@ -6,10 +6,26 @@ function MockCall:new(name, arguments)
 	call.arguments = arguments
 	setmetatable(call, self)
 	self.__index = self
+	
+	return call
 end
 
 function MockCall:isSame(name, arguments)
-	return self.name = name
+	return self.name == name and self:isSameArguments(arguments)
+end
+
+function MockCall:isSameArguments(arguments)
+	if #arguments ~= #self.arguments then
+		return false
+	end
+	
+	for key, value in ipairs(self.arguments) do
+		if arguments[key] ~= value then
+			return false
+		end
+	end
+	
+	return true
 end
 
 Mock = {}
@@ -21,10 +37,29 @@ function Mock:new()
 	return mock
 end
 
+function Mock:addCall(name, arguments)
+	table.insert(self.calls, MockCall:new(name, arguments))
+end
+
+function Mock:hasCall(name, arguments)
+	for i, call in ipairs(self.calls) do
+		if call:isSame(name, arguments) then
+			return true
+		end
+	end
+	
+	return false
+end
+
+function Mock:getCallCount()
+	return table.getn(self.calls)
+end
+
 function Mock:__index(property)
 	local mock = self
+	
 	return function(...)
-		mock.calls[property] = true
+		Mock.addCall(mock, property, arg)
 	end
 end
 
@@ -40,10 +75,18 @@ end
 function Verifier:__index(property)
 	local verifier = self
 	return function(...)
-		return verifier.mock.calls[property] == true
+		return Mock.hasCall(verifier.mock, property, arg)
 	end
 end
 
 function verify(mock)
 	return Verifier:new(mock)
+end
+
+function assertTrue(value)
+	assert(value, 'Expected true was ' .. (value == nil and 'nil' or 'false'))
+end
+
+function assertFalse(value)
+	assert(not value, 'Expected false was true')
 end
--- test_lust.lua
+++ test_lust.lua
@@ -1,6 +1,59 @@
 require 'luaunit/luaunit'
 require 'lust'
 
+TestEndsWith = {}
+
+function string:endsWith(str)
+	local start, stop = self:find(str, self:len() - str:len())
+	return start ~= nil and stop == self:len()
+end
+
+function TestEndsWith:testDoesntEndWith()
+	local str = 'Blah blah blah'
+	assert(not str:endsWith('blahe'))
+	assert(not str:endsWith('Blah'))
+end
+
+function TestEndsWith:testDoesEndWith()
+	local str = 'Blah blah blah'
+	assert(str:endsWith('blah'))
+	
+	local str = 'Another test'
+	assert(str:endsWith('r test'))
+end
+
+TestAssert = {}
+
+function TestAssert:testAssertTrueWithTrue()
+	assert(pcall(assertTrue, true) == true)
+end
+
+function TestAssert:testAssertTrueWithFalse()
+	local status, err = pcall(assertTrue, false)
+	assert(not status)
+	assert(err:endsWith('Expected true was false'))
+end
+
+function TestAssert:testAssertTrueWithNil()
+	local status, err = pcall(assertTrue, nil)
+	assert(not status)
+	assert(err:endsWith('Expected true was nil'))
+end
+
+function TestAssert:testAssertFalseWithFalse()
+	assert(pcall(assertFalse, false) == true)
+end
+
+function TestAssert:testAssertFalseWithTrue()
+	local status, err = pcall(assertFalse, true)
+	assert(not status)
+	assert(err:endsWith('Expected false was true'))
+end
+
+function TestAssert:testAssertFalseWithNil()
+	assert(pcall(assertFalse, nil) == true)
+end
+
 TestMock = {}
 
 function TestMock:setUp()
@@ -12,13 +65,41 @@ function TestMock:testExists()
 end
 
 function TestMock:testVerifyHasntHappened()
-	assert(verify(self.mock).simpleMethod() == false)
+	assertFalse(verify(self.mock).simpleMethod())
 end
 
 function TestMock:testVerifyHasHappened()
 	self.mock.simpleMethod()
-	assert(verify(self.mock).simpleMethod() == true)
+	assertTrue(verify(self.mock).simpleMethod())
 end
 
-TestFunctions = wrapFunctions('test')
+function TestMock:testVerifyHasntHappenedWithArg()
+	assertFalse(verify(self.mock).argMethod(1))
+end
+
+function TestMock:testVerifyHasHappenedWithArg()
+	self.mock.argMethod(1)
+	assertTrue(verify(self.mock).argMethod(1))
+end
+
+function TestMock:testVerifyHasHappenedWithMultipleArgs()
+	self.mock.argMethod(1, 2)
+	assertTrue(verify(self.mock).argMethod(1, 2))
+end
+
+function TestMock:testVerifyHasntHappenedWithDifferentArg()
+	self.mock.argMethod(2)
+	assertFalse(verify(self.mock).argMethod(1))
+end
+
+function TestMock:testVerifyHasntHappenedWithMoreArgs()
+	self.mock.argMethod(2)
+	assertFalse(verify(self.mock).argMethod(2, 1))
+end
+
+function TestMock:testVerifyHasntHappenedWithLessArgs()
+	self.mock.argMethod(2, 1)
+	assertFalse(verify(self.mock).argMethod(2))
+end
+
 LuaUnit:run()
